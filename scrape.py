0303a88e33709bd51ba63feb7b665e11f93dd43c
Gregory H Eales <gregoeales@aol.com>
2021-02-08 12:37:09 -0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,31 +1,19 @@
-import hashlib
-from hashlib import sha256
-import requests
+from utils import *
 
-
-# SHA256(SHA256(previous block hash + merkle root + nonce)) -> block hash
-
-
-url = "https://blockchain.info/rawblock/${}"
-
-
-def scrape_data():
-	pass
-
-def double_sha(block, phash, merkle, nonce):
-	
-	s = merkle+phash#+nonce #block+
-	hash1 = sha256()
-	hash1.update(s)
-	hash2 = sha256()
-	hash2.update(hash1.digest())
-	
-	return hash2.hexdigest()
-
-print(double_sha(0, prev_hash, merkle_root, nonce))
 
 
 def main():
+	
+	# get block hashes
+	hashes = get_block_hashes()
+
+	# save hashes
+	save_block_hashes(hashes)
+
+	# use hashes to get block data
+
+	# save block data as torch dataset
+
 	pass
 
 if __name__ == "__main__":
