53b479a66082fd4288c1b4abccf3964f5470cf2e
Gregory H Eales <gregoeales@aol.com>
2021-02-08 14:30:30 -0800

    added block data scraping

--- scrape.py
+++ scrape.py
@@ -5,16 +5,19 @@ from utils import *
 def main():
 	
 	# get block hashes
-	hashes = get_block_hashes()
+	#hashes = get_block_hashes()
 
 	# save hashes
-	save_block_hashes(hashes)
+	#save_block_hashes(hashes)
+
+	hashes = load_block_hashes()
 
 	# use hashes to get block data
+	data = get_block_data(hashes)
 
 	# save block data as torch dataset
+	save_block_data(data)
 
-	pass
 
 if __name__ == "__main__":
 	main()
--- utils/scrape.py
+++ utils/scrape.py
@@ -1,23 +1,22 @@
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import requests
+import json
 import sys
-
-
 
 # SHA256(SHA256(previous block hash + merkle root + nonce)) -> block hash
 
 
 hashes_url = "https://www.blockchain.com/btc/blocks?page={}"
 
-block_url = "https://blockchain.info/rawblock/${}"
+block_url = "https://blockstream.info/api/block/{}"
 
 
 def get_block_hashes():
 
 	hashes = []
 	
-	for i in tqdm(range(1, 13395+1)):
+	for i in tqdm(range(1, 100)):#13395+1)):
 		r = requests.get(hashes_url.format(1))
 		soup = BeautifulSoup(r.text, 'html.parser')
 		links = soup.findAll('a')
@@ -38,4 +37,31 @@ def save_block_hashes(hashes, path="./data/hashes.txt"):
 		for h in hashes:
 
 			file.write("{}\n".format(h))
+
+
+def load_block_hashes(path="./data/hashes.txt"):
+
+	hashes = []
+
+	with open(path, 'r') as file:
+		for line in file.readlines():
+			hashes.append(line.strip('\n'))
+
+	return hashes
+
+def get_block_data(hashes):
+
+	data = {}
+	data['data'] = []
+
+	for i in tqdm(range(len(hashes))):
+		r = requests.get(block_url.format(hashes[i]))
+		data['data'].append(r.json())
+
+	return data
+
+def save_block_data(data):
+	with open('data.txt', 'w') as file:
+		json.dump(data, file)
+
 	
