cb4797e80aba90594119a3f2255b98afb8c02b68
Gregory H Eales <gregoeales@aol.com>
2021-04-18 15:49:19 -0700

    update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,27 +1,35 @@
 from utils import *
-
+import time
 
 
 def main():
-	
+
+
+	#t = time.time()
+	#hashes = threaded_get_block_hashes(4000)#13395+1)
+	#print(time.time()-t)
 	# get block hashes
 	#hashes = get_block_hashes()
 
 	# save hashes
 	#save_block_hashes(hashes)
-	#hashes = load_block_hashes()
 
+	hashes = load_block_hashes()
+
+	#save_block_hashes(list(set(hashes)))
+	t = time.time()
 	# use hashes to get block data
-	#data = get_block_data(hashes)
+	data = get_block_data(hashes[0:4000])
+	print(time.time()-t)
 
 	# save block data as torch dataset
-	#save_block_data(data)
+	save_block_data(data)
 
 	# load block data
-	data = load_block_data()
+	#data = load_block_data()
 
-	x, y = generate_data(data['data'])
-	save_tensors(x, y)
+	#x, y = generate_data(data['data'])
+	#save_tensors(x, y)
 
 	"""
 	for d in data['data']:
--- utils/scrape.py
+++ utils/scrape.py
@@ -1,14 +1,15 @@
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import requests
 import json
 import sys
+import threading
+import time
 
 # SHA256(SHA256(previous block hash + merkle root + nonce)) -> block hash
 
-
 hashes_url = "https://www.blockchain.com/btc/blocks?page={}"
-
 block_url = "https://blockstream.info/api/block/{}"
 
 
@@ -16,8 +17,8 @@ def get_block_hashes():
 
 	hashes = []
 	
-	for i in tqdm(range(1, 100)):#13395+1)):
-		r = requests.get(hashes_url.format(1))
+	for i in tqdm(range(1, 13395+1)):
+		r = requests.get(hashes_url.format(i))
 		soup = BeautifulSoup(r.text, 'html.parser')
 		links = soup.findAll('a')
 		links = [l.get('href') for l in links]
@@ -27,6 +28,39 @@ def get_block_hashes():
 				hashes.append(l[11:])
 
 	return hashes
+
+def get_block_hash(i):
+
+	print(i)
+
+	hashes = []
+
+	try:
+
+		r = requests.get(hashes_url.format(i))
+		soup = BeautifulSoup(r.text, 'html.parser')
+		links = soup.findAll('a')
+		links = [l.get('href') for l in links]
+		for l in links:
+			url = '/btc/block/'
+			if l[0:len(url)] == url:
+				hashes.append(l[11:])
+
+	except:
+		pass
+
+	return hashes
+
+
+def threaded_get_block_hashes(n, n_threads=20):
+	p = Pool(processes=n_threads)
+	data = p.map(get_block_hash,[i for i in range(1, n)])
+	p.close()
+	l = []
+	for d in data:
+		l += d
+
+	return l
 
 
 def save_block_hashes(hashes, path="./data/hashes.txt"):
@@ -49,14 +83,20 @@ def load_block_hashes(path="./data/hashes.txt"):
 	return hashes
 
 
-def get_block_data(hashes):
+
+
+def get_block(h):
+	r = requests.get(block_url.format(h))
+	return r.json()
+	
+
+def get_block_data(hashes, n=5):
 
 	data = {}
 	data['data'] = []
 
 	for i in tqdm(range(len(hashes))):
-		r = requests.get(block_url.format(hashes[i]))
-		data['data'].append(r.json())
+		data['data'].append(get_block(hashes[i]))
 
 	return data
 
@@ -72,6 +112,7 @@ def load_block_data(path='./data/data.txt'):
 		data = json.load(file)
 
 	return data
+
 
 
 def generate_dataset(data):
